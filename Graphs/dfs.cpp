542b121052b977af8513acf5038b6727624c03eb
Akshayshahi900 <shahiakshay900@gmail.com>
2025-08-03 23:15:03 +0530

    Heaps

---
+++ Graphs/dfs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+class Solution
+{
+private:
+    void dfs(int node, vector<vector<int>> &adj, int vis[], vector<int> &ls)
+    {
+        vis[node] = 1;
+        ls.push_back(node);
+        // traverse all its neighbours/adjacent elements
+
+        for (auto it : adj[node])
+        {
+            if (!vis[it])
+            {
+                dfs(it, adj, vis, ls);
+            }
+        }
+    }
+
+public:
+    vector<int> dfs(vector<vector<int>> &adj)
+    {
+        // Code here
+        int vis[adj.size()] = {0};
+        int start = 0;
+        vector<int> ls;
+        dfs(start, adj, vis, ls);
+        return ls;
+    }
+};
