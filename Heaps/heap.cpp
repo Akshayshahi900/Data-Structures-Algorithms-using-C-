1d93b73940b5d535bc15bb585d95b88e15fe7a27
Akshayshahi900 <shahiakshay900@gmail.com>
2025-08-04 22:10:59 +0530

    Heaps

--- Heaps/heap.cpp
+++ Heaps/heap.cpp
@@ -57,7 +57,37 @@ class Heap{
         }
         cout << endl;
     }
+    
 };
+void heapify(int arr[], int n  , int i ){
+        int largest = i;
+        int left = 2*i;
+        int right =2*i +1;
+        if(left <= n && arr[largest] < arr[left]){
+            largest = left;
+        }
+        if(right <= n && arr[largest] < arr[right]){
+            largest = right;
+        }
+        if(largest != i){
+            swap(arr[largest] , arr[i]);
+            heapify(arr, n, largest);
+        }
+}
+void heapsort(int arr[] , int n){
+    int size = n;
+    while(size >1){
+        // Step -1
+        swap(arr[size] , arr[1]);
+        size--;
+
+        //Step -2
+        heapify(arr,size , 1);
+
+
+    }
+}
+
 int main(){
  Heap h;
  h.insert(50);
@@ -66,7 +96,28 @@ int main(){
  h.insert(52);
  h.insert(54);
 
- h.print();
+//  h.print();
  h.deletefromHeap();
- h.print();
+//  h.print();
+
+ int arr[6] = {-1, 54,53 , 55, 52 , 50};
+    int n =5;
+    for(int i = n/2 ; i >=1 ; i--){
+        heapify(arr, n , i);
+    }
+    
+    heapsort(arr, 5);
+    cout <<"Printing the array after heap sort" <<endl;
+    for(int i =1 ; i <=n ; i++){
+        cout <<arr[i] << " " ;
+    }
+    cout <<endl;
+    priority_queue<int> pq;
+    pq.push(4);
+    pq.push(3);
+    pq.push(33);
+    pq.push(44);
+
+ priority_queue<int, vector<int>, greater<int>> minHeap;
+    return 0;
 }
